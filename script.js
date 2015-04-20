86cd9b893e9e694805fd75648876ec1142096863
projektADSU <anjahauptman1@gmail.com>
2015-04-20 21:46:33 +0200

    Začetna verzija

---
+++ script.js
@@ -0,0 +1,37 @@
+/*global $, jQuery, alert*/
+$(document).ready(function () {
+    "use strict";
+    $('.tabs .tab-links a').on('click', function (e) {
+        var currentAttrValue = $(this).attr('href');
+ 
+        // Show/Hide Tabs
+        $('.tabs ' + currentAttrValue).show().siblings().hide();
+ 
+        // Change/remove current tab to active
+        $(this).parent('li').addClass('active').siblings().removeClass('active');
+ 
+        e.preventDefault();
+    });
+    
+    $('.search-opt a').on('click', function (e) {
+        var currentAttrValue = $(this).attr('href');
+        
+        if ($('.search-opt ' + currentAttrValue).hasClass('active') === false) {
+            $('.search-opt ' + currentAttrValue).addClass('active');
+            $('.search-opt ' + currentAttrValue).parent('li').toggleClass('active');
+        } else {
+            $('.search-opt ' + currentAttrValue).removeClass('active');
+            $('.search-opt ' + currentAttrValue).parent('li').toggleClass('active');
+        }
+        
+        e.preventDefault();
+    });
+    
+    $(document).delegate('input:text', 'keypress', function (e) {
+        if (e.which === 13) { // if is enter
+            alert("Search trigger");
+        }
+    });
+    
+});
+
